884731ee77472ba6e5a7f4308bc72e2309c1ca8a
Hagenau <hagenau.andersen@gmail.com>
2022-10-09 17:51:22 +0200

    Bug fix

--- components/Community.tsx
+++ components/Community.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 type Props = {}
 
 function Community({}: Props) {
-  const projects = [1, 2, 3, 4, 5];
   return (
     <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row
                     max-w-full justify-evenly mx-auto items-center z-0'>
@@ -14,7 +13,6 @@ function Community({}: Props) {
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x
                         snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20
                         scrollbar-thumb-[#F7AB0A]/80'>
-          {projects.map((project, i) => (
             <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5
                             items-center justify-center p-20 md:p-44 h-screen'>
               <img className='xl:w-[500px] xl:h-[500px] object-scale-down object-center'
@@ -24,7 +22,7 @@ function Community({}: Props) {
 
               <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                 <h4 className='text-4xl font-semibold text-center'>
-                  <span className='underline decoration-[#F7AB0A]/50'>Project {i+1} of {projects.length}:</span> Dette er mit projekt
+                  <span className='underline decoration-[#F7AB0A]/50'>Project 1 of 5:</span> Dette er mit projekt
                 </h4>
                 <p> 
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
@@ -35,7 +33,6 @@ function Community({}: Props) {
                 </p>
               </div>
             </div>
-          ))}
         </div>
 
         <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] 
