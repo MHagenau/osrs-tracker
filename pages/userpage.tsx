e8a47f70af0b47019994df425cc81df922332a58
Morten Hagenau <hagenau.andersen@gmail.com>
2023-02-28 21:05:04 +0100

    Added overview/gained dropdown to player page

--- pages/userpage.tsx
+++ pages/userpage.tsx
@@ -12,12 +12,13 @@ import { motion } from "framer-motion"
 
 const PlayerPage: NextPage = () => {
 
-  const [selects, setSelects] = useState("Skills")
-  function playerTable(drpdownInput: string){
+  const [selectsStatType, setSelectsStatType] = useState("Skills")
+  const [selectsType, setSelectsType] = useState("Overview")
 
-    if (drpdownInput === 'Skills')
+  function playerTable(drpdownInputStatType: string, drpdownInputType: string){
+    if (drpdownInputStatType === 'Skills')
       return <SkillTable stats={statsJson} />
-    else if (drpdownInput === 'Bosses')
+    else if (drpdownInputStatType === 'Bosses')
       return <BossTable stats={statsJson} />
     else
       return <MinigameTable stats={statsJson} />
@@ -51,9 +52,14 @@ const PlayerPage: NextPage = () => {
         viewport={{ 
             once: true
         }}>
-          <div className='flex pt-5 pb-5 justify-center items-center'>
-            <select className='block w-1/5 p-2 pl-2 pr-2 text-s text-gray-400 bg-[#1f1f1f] rounded-lg'
-                    value={selects} onChange={e => setSelects(e.target.value)}>
+          <div className='flex w-1/3 pt-5 pb-5 justify-center items-center mx-auto'>
+            <select className='w-3/5 p-2 mr-5 text-gray-400 bg-[#1f1f1f] rounded-lg'
+                    value={selectsType} onChange={e => setSelectsType(e.target.value)}>
+              <option>Overview</option>
+              <option>Gained</option>
+            </select>
+            <select className='w-3/5 p-2 ml-5 text-gray-400 bg-[#1f1f1f] rounded-lg'
+                    value={selectsStatType} onChange={e => setSelectsStatType(e.target.value)}>
               <option>Skills</option>
               <option>Bosses</option>
               <option>Minigames</option>
@@ -61,7 +67,7 @@ const PlayerPage: NextPage = () => {
           </div>
 
           <section>
-            {playerTable(selects)}
+            {playerTable(selectsStatType, selectsType)}
           </section>
 
       </motion.div>
