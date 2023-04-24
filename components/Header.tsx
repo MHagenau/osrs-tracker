cc4ce7cf4a648e4a54d51eeafbcaa69e0a6f4834
Morten Hagenau <moha@frb-forsyning.dk>
2023-04-24 10:14:21 +0200

    Added api call to header search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link';
 import { motion } from "framer-motion";
 
 type Props = {}
 
 export default function Header({}: Props) {
+    const [username, setUsername] = useState('');
+    const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        setUsername(event.target.value);
+      }
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
         <motion.div 
@@ -31,8 +35,16 @@ export default function Header({}: Props) {
                 <div className="relative">
                     <input className="block p-2.5 pl-4 w-full text-xs text-gray-300 bg-[#333333] rounded-lg
                                     focus:ring-[#F7AB0A]/80 focus:border-[#F7AB0A]/80 focus:outline-none"
-                            placeholder="Lookup player ..."/>
-                    <Link href="/userpage">
+                            placeholder="Lookup player ..."
+                            type="text"
+                            id="username"
+                            name="username"
+                            onChange={handleChange}
+                            value={username}/>
+                    <Link href={{
+                        pathname: '/userpage',
+                        query: {user: username},
+                        }}>
                     <button formAction="/userpage" className="text-white absolute right-1.5 bottom-1.5 bg-[#F7AB0A]/20 hover:bg-[#F7AB0A]/50 
                             focus:ring-2 focus:ring-[#F7AB0A]/80 focus:outline-none font-medium rounded-lg text-xs px-4 py-1">
                     Lookup
